81c2831313fd3860a34335f368bb5b75936f6ee2
TekuSP <richard.torhan@windowslive.com>
2021-04-19 15:48:16 +0200

    Remove not used WMI references, add MSI System WMI

--- SubZero/Models/MSIWmiHelper.cs
+++ SubZero/Models/MSIWmiHelper.cs
@@ -18,11 +18,8 @@ namespace SubZero.Models
             MSI_CPU = new ManagementObjectSearcher("root\\WMI", "SELECT * FROM MSI_CPU");
             MSI_GPU = new ManagementObjectSearcher("root\\WMI", "SELECT * FROM MSI_VGA");
             MSI_AP = new ManagementObjectSearcher("root\\WMI", "SELECT AP FROM MSI_AP");
-            MSI_Master_Battery = new ManagementObjectSearcher("root\\WMI", "SELECT Master_Battery FROM MSI_Master_Battery");
-            MSI_Power = new ManagementObjectSearcher("root\\WMI", "SELECT Power FROM MSI_Power");
             MSI_LaptopModel = new ManagementObjectSearcher("root\\CIMV2", "SELECT Model FROM Win32_ComputerSystem");
-            WIN_PowerPlan = new ManagementObjectSearcher("root\\CIMV2\\power", "SELECT * FROM Win32_PowerPlan"); //TODO: ???
-            WIN_DisplayConfig = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DisplayConfiguration"); //TODO: ???
+            MSI_System = new ManagementObjectSearcher("root\\WMI", "SELECT * FROM MSI_System");
             //Test if they are valid
             using (ManagementObjectCollection test = MSI_CPU.Get())
             {
@@ -35,26 +32,14 @@ namespace SubZero.Models
             using (ManagementObjectCollection test = MSI_AP.Get())
             {
                 IsAvailableMSI_AP = test.Count != 0;
-            }
-            using (ManagementObjectCollection test = MSI_Master_Battery.Get())
-            {
-                IsAvailableMSI_Master_Battery = test.Count != 0;
-            }
-            using (ManagementObjectCollection test = MSI_Power.Get())
-            {
-                IsAvailableMSI_Power = test.Count != 0;
-            }
+            }            
             using (ManagementObjectCollection test = MSI_LaptopModel.Get())
             {
                 IsAvailableMSI_LaptopModel = test.Count != 0;
             }
-            using (ManagementObjectCollection test = WIN_PowerPlan.Get())
+            using (ManagementObjectCollection test = MSI_System.Get())
             {
-                IsAvailableWIN_PowerPlan = test.Count != 0;
-            }
-            using (ManagementObjectCollection test = WIN_DisplayConfig.Get())
-            {
-                IsAvailableWIN_DisplayConfig = test.Count != 0;
+                IsAvailableMSI_System = test.Count != 0;
             }
         }
 
@@ -83,24 +68,9 @@ namespace SubZero.Models
         public bool IsAvailableMSI_LaptopModel { get; private set; }
 
         /// <summary>
-        /// Is MSI Master Battery Available to be used?
+        /// Is MSI System Available to be used?
         /// </summary>
-        public bool IsAvailableMSI_Master_Battery { get; private set; }
-
-        /// <summary>
-        /// Is MSI Power Available to be used?
-        /// </summary>
-        public bool IsAvailableMSI_Power { get; private set; }
-
-        /// <summary>
-        /// Is Windows DisplayConfig Available to be used?
-        /// </summary>
-        public bool IsAvailableWIN_DisplayConfig { get; private set; }
-
-        /// <summary>
-        /// Is Windows PowerPlan Available to be used?
-        /// </summary>
-        public bool IsAvailableWIN_PowerPlan { get; private set; }
+        public bool IsAvailableMSI_System { get; private set; }
 
         /// <summary>
         /// AP WMI
@@ -123,24 +93,9 @@ namespace SubZero.Models
         public ManagementObjectSearcher MSI_LaptopModel { get; private set; }
 
         /// <summary>
-        /// Master Battery WMI
+        /// MSI System WMI
         /// </summary>
-        public ManagementObjectSearcher MSI_Master_Battery { get; private set; }
-
-        /// <summary>
-        /// Power WMI
-        /// </summary>
-        public ManagementObjectSearcher MSI_Power { get; private set; }
-
-        /// <summary>
-        /// Windows Display WMI, do we need this?
-        /// </summary>
-        public ManagementObjectSearcher WIN_DisplayConfig { get; private set; }
-
-        /// <summary>
-        /// Windows PowerPlan WMI, do we need this?
-        /// </summary>
-        public ManagementObjectSearcher WIN_PowerPlan { get; private set; }
+        public ManagementObjectSearcher MSI_System { get; private set; }
 
         #endregion Public Properties
     }
