c456a038274c399d54762cd725d579832ac99d2d
TekuSP <richard.torhan@windowslive.com>
2021-04-19 14:52:53 +0200

    Cleanup

--- SubZero/Models/MSIHardwareMonitor.cs
+++ SubZero/Models/MSIHardwareMonitor.cs
@@ -1,37 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SubZero.Models
+﻿namespace SubZero.Models
 {
     /// <summary>
     /// Monitors MSI Laptop Hardware
     /// </summary>
     public class MSIHardwareMonitor
     {
-        /// <summary>
-        /// WMI Helper to use
-        /// </summary>
-        private MSIWmiHelper MSIWmiHelper { get; }
-        /// <summary>
-        /// Fan Controller
-        /// </summary>
-        private Hardware.MSIFans FanController { get; }
-        private Hardware.MSITemperatureSensors TemperatureSensors { get; }
+        #region Public Constructors
+
         /// <summary>
         /// Initializes MSI Hardware monitoring
         /// </summary>
         /// <param name="wmiHelper">WMI Helper to use</param>
         public MSIHardwareMonitor(MSIWmiHelper wmiHelper)
         {
-            MSIWmiHelper = wmiHelper;
             FanController = new Hardware.MSIFans(wmiHelper);
-            FanController.StartMonitor(TimeSpan.FromMilliseconds(500));
             TemperatureSensors = new Hardware.MSITemperatureSensors(wmiHelper);
         }
 
+        #endregion Public Constructors
 
+        #region Public Properties
+
+        //TODO: Move to different DLL, and add more functionality for all MSI hidden APIs
+        /// <summary>
+        /// Fan Controller
+        /// </summary>
+        public Hardware.MSIFans FanController { get; }
+
+        /// <summary>
+        /// Temperature Sensors
+        /// </summary>
+        public Hardware.MSITemperatureSensors TemperatureSensors { get; }
+
+        #endregion Public Properties
     }
 }
